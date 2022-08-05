72919fdb362a8276c33b59d601e3a9c477a64ab9
nlo88 <nloyeltylo@gmail.com>
2022-08-05 12:33:07 -0400

    reverse linked list

---
+++ LinkedList/ReverseLinkedList.js
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+
+var reverseList = (head) => {
+    
+    let currentNode = head;
+    let temp = null;
+        
+    while ( currentNode ) {
+        let nextNode = currentNode.next;
+        currentNode.next = temp;
+        temp = currentNode;
+        currentNode = nextNode;
+        
+    }
+    return temp
+    
+   
+    
+};
