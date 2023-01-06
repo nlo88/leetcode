70618ad6a4995f46084d42a6b7385052cfc64cd3
nlo88 <nloyeltylo@gmail.com>
2023-01-05 19:39:10 -0500

    buyandsellstock

--- Arrays/Easy/BuyandSellStock.js
+++ Arrays/Easy/BuyandSellStock.js
@@ -86,3 +86,43 @@ max value - min value = profit
 //   }
 // };
 // maxProfit(prices);
+
+
+// var maxProfit = function (prices){
+    
+    //     let left = 0; 
+    //     let right = 1;
+    
+    //     let max = 0;
+    
+    //     while (right < prices.length){
+    //         if (prices[left] < prices[right]) {
+    //             let diff = prices[right] - prices[left]
+    //             max = Math.max(diff, max)
+    //         } else {
+    //             left = right
+    //         }
+    //         right++
+    //     }   
+    //     return max
+    // }
+    
+
+    var maxProfit = function(prices){
+        let max = 0;
+
+        let left = 0; let right= 0;
+
+        while(right < prices.length){
+            if(prices[left] > prices[right]){
+                left = right;
+            }
+            max = Math.max(max, prices[right] - prices[left])
+            right++
+        }
+
+        return max;
+    }
+
+console.log(maxProfit([7,1,5,3,6,4], 5))
+console.log(maxProfit([7,6,4,3,1], 0))
